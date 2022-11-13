afb826daf9cb88b38eeefc769fb15243b287e46d
magicaljake <boobyvils@gmail.com>
2022-11-13 18:20:41 +0300

    Initial commit

---
+++ src/main/java/com/anistory/anistory/AnistoryApplication.java
@@ -0,0 +1,13 @@
+package com.anistory.anistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anistory/anistory/AnistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anistory.anistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
